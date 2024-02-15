b49e55e77c42417f0c7aa80fc536551ecd922452
Vivek Goel <vivek@ncompass.inc>
2024-02-15 12:48:03 +0530

    Added create new blog

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request, Res } from '@nestjs/common';
 import { BlogsService } from './blogs.service';
+import { CreateBlogDto } from './dto/createBlog.dto';
+import { ApiResponse } from 'src/utils/response';
 
-@Controller()
+@Controller("blogs")
 export class BlogsController {
   constructor(private blogsService : BlogsService) {}
+
+  @Post()
+  createBlog(@Body() createBlogDto:CreateBlogDto, @Res() response: Response, @Request() request){
+    const createBlogData = this.blogsService.createBlog({...createBlogDto, blog_owner: request.payload.username})
+    new ApiResponse(response, 200, {messgae: "Blog Created"})
+  }
 }
--- src/blogs/blogs.service.ts
+++ src/blogs/blogs.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/users/entity/users.entity';
+import { Repository } from 'typeorm';
+import { Blogs } from './entity/blogs.entity';
+import { CreateBlogParams } from './types/createBlog.type';
+import { Topics } from 'src/topic/entity/topic.entity';
+import { CustomError } from 'src/utils/customError';
 
 @Injectable()
-export class BlogsService {}
+export class BlogsService {
+  constructor(
+    @InjectRepository(Users) private usersRepository: Repository<Users>,
+    @InjectRepository(Blogs) private blogsRepository: Repository<Blogs>,
+    @InjectRepository(Topics) private topicsRepository: Repository<Topics>,
+  ) {}
+
+  async createBlog(createBlogParams : CreateBlogParams){
+    const topicDetails = await this.topicsRepository.findOne({where: {topic_name:createBlogParams.topic}, relations:["topics"]})
+    
+    const editors = topicDetails.editors
+
+    let presentAsEditor = false 
+    for(let i = 0; i<editors.length; i++){
+      if(editors[i].username === createBlogParams.blog_owner){
+        presentAsEditor = true
+        break
+      }
+    }
+
+    if(!presentAsEditor){
+      throw new CustomError(HttpStatus.BAD_REQUEST, {message:"The user is not a editor and cannot create a blog"})
+    }
+
+    const blogDetails = this.blogsRepository.create(createBlogParams)
+
+    return await this.blogsRepository.save(blogDetails)
+
+  }
+}
