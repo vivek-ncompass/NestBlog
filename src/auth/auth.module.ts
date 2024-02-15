29219fbeaa37142f33cea71273192a04481d4a0b
Vivek Goel <vivek@ncompass.inc>
2024-02-15 15:16:26 +0530

    made editor check guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Topics } from 'src/topic/entity/topic.entity';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Topics])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
