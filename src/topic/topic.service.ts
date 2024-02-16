5384ac96afd1646396ea206f1ea483b43aab466b
Vivek Goel <vivek@ncompass.inc>
2024-02-17 00:35:16 +0530

    created createTopic guard

--- src/topic/topic.service.ts
+++ src/topic/topic.service.ts
@@ -19,34 +19,28 @@ export class TopicService {
     return await this.usersRepository.findOne({ where: { username: user } });
   }
 
-  async createTopic(level, createTopicParams: CreateTopicParams) {
-    if (level <= 2) {
-      throw new CustomError(HttpStatus.BAD_REQUEST, {
-        message: 'Cannot create topic as you dont have the permission to do so',
-      });
-    } else {
+  async createTopic(createTopicParams: CreateTopicParams) {
       
-      let editorsDataArr = [null], viewersDataArr = [null]
+    let editorsDataArr = [null], viewersDataArr = [null]
 
-      for(let i = 0; i<createTopicParams.editors.length; i++){
-        editorsDataArr.push(await this.findUserByUsername(createTopicParams.editors[i]))
-      }
-      
-      for(let i = 0; i<createTopicParams.viewers.length; i++){
-        viewersDataArr.push(await this.findUserByUsername(createTopicParams.viewers[i]))
-      }
-
-      const createTopicData = this.topicsRepository.create({
-        topic_name: createTopicParams.topic_name,
-        desc: createTopicParams.desc,
-        topic_owner: createTopicParams.topic_owner,
-        editors: editorsDataArr,
-        viewers: viewersDataArr,
-        blogs: null,
-      });
-
-      return this.topicsRepository.save(createTopicData);
+    for(let i = 0; i<createTopicParams.editors.length; i++){
+      editorsDataArr.push(await this.findUserByUsername(createTopicParams.editors[i]))
     }
+    
+    for(let i = 0; i<createTopicParams.viewers.length; i++){
+      viewersDataArr.push(await this.findUserByUsername(createTopicParams.viewers[i]))
+    }
+
+    const createTopicData = this.topicsRepository.create({
+      topic_name: createTopicParams.topic_name,
+      desc: createTopicParams.desc,
+      topic_owner: createTopicParams.topic_owner,
+      editors: editorsDataArr,
+      viewers: viewersDataArr,
+      blogs: null,
+    });
+
+    return this.topicsRepository.save(createTopicData);
   }
 
   async updateTopic(id:number, updateTopicParams: UpdateTopicParams){
